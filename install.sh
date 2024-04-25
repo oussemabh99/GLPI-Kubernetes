d2b7faffbb5527e8cfaba4f28b230798c29ce39d
oussemabh99 <oussema.benhdid@etudiant-enit.utm.tn>
2024-04-25 09:54:41 +0000

    mise a jour au install 1

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 git config --global user.name "oussemabh99"
 git config --global user.email "oussema.benhdid@etudiant-enit.utm.tn"
+#kubectl create -f ${pwd}/pods/namespace.yaml
+#kubectl create -f ${Deployments}/k.yaml
+#kubectl create -f ${Service}/.yaml
