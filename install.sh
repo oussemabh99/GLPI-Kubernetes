e8481671b76306e7394a6b76d8808a10c80fb4dd
oussemabh99 <oussema.benhdid@etudiant-enit.utm.tn>
2024-05-02 09:53:42 +0000

    mise a jour au fichier install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 git config --global user.name "oussemabh99"
 git config --global user.email "oussema.benhdid@etudiant-enit.utm.tn"
-#kubectl create -f ${pwd}/pods/namespace.yaml
-#kubectl create -f ${Deployments}/k.yaml
-#kubectl create -f ${Service}/.yaml
+kubectl create -f $(pwd)/pods/namespace.yaml;
+kubectl create -f $(pwd)/secrets/secret1.yaml;
+kubectl create -f $(pwd)/Deployments/kube-registry-2.yaml;
+kubectl create -f $(pwd)/Service/kube-regitry.yaml;
+echo "test de connectivité";
+curl -v localhost:32000;
+
