e558ccaefd5ab4fb72094e30a5fde2e0de4e86f4
oussemabh99 <oussema.benhdid@etudiant-enit.utm.tn>
2024-05-02 11:02:45 +0000

    automated install.sh script

--- install.sh
+++ install.sh
@@ -4,6 +4,15 @@ kubectl create -f $(pwd)/pods/namespace.yaml;
 kubectl create -f $(pwd)/secrets/secret1.yaml;
 kubectl create -f $(pwd)/Deployments/kube-registry-2.yaml;
 kubectl create -f $(pwd)/Service/kube-regitry.yaml;
-echo "test de connectivité";
-curl -v localhost:32000;
+sleep 10 ;
+if ( curl -v  localhost:32000); then
+   if (docker login localhost:32000); then
+    docker pull alpine && docker tag alpine:latest localhost:32000/myos:v1 && docker push localhost:32000/myos:v1 &&
+    kubectl create secret generic auth --from-file .dockerconfigjson=/root/.docker/config.json --type kubernetes.io/dockerconfigjson -n devops && 
+    kubectl create -f $(pwd)/Deployments/app1.yaml ;
+   else 
+    echo "ereur";
+   fi
+  
+fi
 
