5e9fbf45d097f7a14cab4f1744ffc0aa0113c3e5
oussemabh99 <oussema.benhdid@etudiant-enit.utm.tn>
2024-05-21 14:56:30 +0000

    update_flask

--- python_app/server.py
+++ python_app/server.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests as rq
 import json
 app = Flask(__name__)
 @app.route("/")
 def index() :
-  r = rq.get('http://kube-local-registry:5000/v2/_catalog', auth=('admin', 'oussemaest19'))
+  r = rq.get('http://localhost:32000/v2/_catalog', auth=('admin', 'oussemaest19'))
   exe = r.text
   test= json.loads(exe)
   i = 0
   for x in test["repositories"]:
     i= i+1
-  return (str(i))
+  return render_template('index.html',data=test)
+
