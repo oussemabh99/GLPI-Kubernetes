2e31759b3134910bde1db3220cde47c58807cca7
oussemabh99 <oussema.benhdid@etudiant-enit.utm.tn>
2024-11-11 13:25:56 +0000

    modify glpi

--- glpi/glpi-install.sh
+++ glpi/glpi-install.sh
@@ -1,7 +1,4 @@
 #/bin/bash
-docker pull oussemabh999/glpi:v1.2
-docker tag oussemabh999/glpi:v1.2  localhost:5000/glpi:v1
-docker rmi oussemabh999/glpi:v1.2
 mkdir -p /srv/files /srv/config /srv/config /srv/marketplace /srv/login/ /srv/mysql /srv/plugin
 mkdir -p /srv/files/_cron /srv/files/_dumps /srv/files/_graphs /srv/files/_lock /srv/files/_pictures /srv/files/_plugins
 mkdir -p /srv/files/_rss /srv/files/_sessions /srv/files/_tmp /srv/files/_uploads
